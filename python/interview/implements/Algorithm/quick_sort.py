d07d69580fa048beb841bbd0517eee0b690011e8
A-janjan <a.m.x.janjan@gmail.com>
2024-06-28 23:26:39 +0330

    quick_sort

---
+++ python/interview/implements/Algorithm/quick_sort.py
@@ -0,0 +1,11 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = [len(arr)//2]
+        
+        right = [x for x in arr if x>pivot]
+        left = [x for x in arr if x<pivot]
+        middle = [x for x in arr if x==pivot]
+        
+        return quick_sort(left) + middle + quick_sort(right)
